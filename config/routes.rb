084d87b83983ae1c74f478c49296a6727b59faf3
gabrielcampos243 <gabrielcampos243@gmail.com>
2022-08-29 18:13:25 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "dashboard", to: "pages#dashboard"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :treatment, only: [:index, :show, :new, :create]
+
+  resources :patients, only: [:new, :create, :show, :report]
 end
