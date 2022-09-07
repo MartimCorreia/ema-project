dff4c72c2d42bb7befbbc4829237f6f8ba2d6894
Martim Correia <correia_martim@hotmail.com>
2022-09-07 10:43:28 +0100

    patient show enrichment

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -29,7 +29,7 @@ class PatientsController < ApplicationController
   private
 
   def patient_params
-    params.require(:patient).permit(:first_name, :last_name, :age, :weight, :occupation, :address, :medical_history)
+    params.require(:patient).permit(:first_name, :last_name, :age, :weight, :occupation, :address, :medical_history, :photo)
   end
 
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,6 @@
 class Patient < ApplicationRecord
   belongs_to :treatment
   belongs_to :user
+  has_one_attached :photo
   has_many :medical_reports, dependent: :destroy
 end
